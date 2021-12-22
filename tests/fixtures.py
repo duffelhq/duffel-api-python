700384a355f26dfa3a93cb32da3f20853c188356
Jesse Claven <jesse@duffel.com>
2021-12-22 15:31:00 +0000

    Add unit test for Offer model

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -7,9 +7,11 @@ from duffel_api import Duffel
 
 @contextmanager
 def fixture(name, url_path, mock, status_code, with_response=True):
-    """Yields a Duffel client instance that responds to a given `url_path` under `mock`"""
+    """Yields a Duffel client instnace.
+
+    It responds to a given `url_path` under `mock`."""
     url = "http://someaddress"
-    with open("tests/fixtures/{}.json".format(name)) as fh:
+    with open(f"tests/fixtures/{name}.json") as fh:
         if with_response:
             response = json.loads(fh.read())
             uri = "{}/{}".format(url, url_path)
@@ -19,3 +21,10 @@ def fixture(name, url_path, mock, status_code, with_response=True):
             uri = "{}/{}".format(url, url_path)
             mock(uri, complete_qs=True, status_code=status_code)
             yield Duffel(access_token="some_token", api_url=url)
+
+
+@contextmanager
+def raw_fixture(name):
+    """Yields an instance of the fixture that has been loaded as JSON."""
+    with open(f"tests/fixtures/{name}.json") as fh:
+        yield json.load(fh)
