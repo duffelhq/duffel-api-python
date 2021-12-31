9b904ab6b615745db421e369fb34f9417bf04b5c
Sasha Gerrand <sasha@duffel.com>
2021-12-31 14:55:36 +0000

    Update OrderChangeRequest data structure
    * Add missing `created_at` and `updated_at`
    * Remove `order_change_offers` from HTTP POST response (only returned by HTTP GET)
    * Fix structure of `slices[].add` and `slices[].remove`

--- tests/test_order_change_requests.py
+++ tests/test_order_change_requests.py
@@ -19,29 +19,22 @@ def test_create_order_change_request(requests_mock):
         }
         creation = client.order_change_requests.create("order-id").slices(slices)
         change_request = creation.execute()
+        assert change_request.created_at == datetime.datetime(
+            2020, 1, 17, 10, 12, 11, 634000
+        )
         assert change_request.id == "ocr_0000A3bQP9RLVfNUcdpLpw"
         assert change_request.live_mode is True
         assert change_request.order_id == "ord_0000A3bQ8FJIQoEfuC07n6"
-        assert isinstance(change_request.order_change_offers, type([]))
-        assert (
-            change_request.order_change_offers[0].slices.add[0].id
-            == "sli_00009htYpSCXrwaB9Dn123"
-        )
-        assert isinstance(change_request.order_change_offers[0].slices.remove, type([]))
-        assert (
-            change_request.order_change_offers[0].slices.remove[0].id
-            == "sli_00009htYpSCXrwaB9Dn123"
-        )
-        assert change_request.order_change_offers[0].updated_at == datetime.datetime(
-            2020, 1, 17, 10, 12, 14, 545000
-        )
         assert isinstance(change_request.slices.add, type([]))
         assert change_request.slices.add[0].cabin_class == "economy"
         assert change_request.slices.add[0].departure_date == datetime.date(2020, 4, 24)
-        assert change_request.slices.add[0].destination.id == "arp_jfk_us"
-        assert change_request.slices.add[0].origin.id == "arp_lhr_gb"
+        assert change_request.slices.add[0].destination == "JFK"
+        assert change_request.slices.add[0].origin == "LHR"
         assert isinstance(change_request.slices.remove, type([]))
         assert change_request.slices.remove[0].slice_id == "sli_00009htYpSCXrwaB9Dn123"
+        assert change_request.updated_at == datetime.datetime(
+            2020, 1, 17, 10, 12, 11, 634000
+        )
 
 
 def test_get_order_change_request(requests_mock):
@@ -50,6 +43,9 @@ def test_get_order_change_request(requests_mock):
         "get-order-change-request-by-id", url, requests_mock.get, 200
     ) as client:
         change_request = client.order_change_requests.get("order-id")
+        assert change_request.created_at == datetime.datetime(
+            2020, 1, 17, 10, 12, 11, 634000
+        )
         assert change_request.id == "ocr_0000A3bQP9RLVfNUcdpLpw"
         assert change_request.live_mode is True
         assert change_request.order_id == "ord_0000A3bQ8FJIQoEfuC07n6"
@@ -69,7 +65,10 @@ def test_get_order_change_request(requests_mock):
         assert isinstance(change_request.slices.add, type([]))
         assert change_request.slices.add[0].cabin_class == "economy"
         assert change_request.slices.add[0].departure_date == datetime.date(2020, 4, 24)
-        assert change_request.slices.add[0].destination.id == "arp_jfk_us"
-        assert change_request.slices.add[0].origin.id == "arp_lhr_gb"
+        assert change_request.slices.add[0].destination == "JFK"
+        assert change_request.slices.add[0].origin == "LHR"
         assert isinstance(change_request.slices.remove, type([]))
         assert change_request.slices.remove[0].slice_id == "sli_00009htYpSCXrwaB9Dn123"
+        assert change_request.updated_at == datetime.datetime(
+            2020, 1, 17, 10, 12, 11, 634000
+        )
