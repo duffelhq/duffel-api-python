39d53e45112f8112830c6db0d9ac140e1caa0221
Jesse Claven <jesse@duffel.com>
2022-02-11 13:23:49 +0000

    Make fields nullable to match our documentation and serialisers
    Going by
    https://duffel.com/docs/api/offers/schema#offers-schema-payment-requirements,
    these are nullable. This wasn't caught in our testing as our test
    fixture has values for these 2 fields.

--- tests/test_offer.py
+++ tests/test_offer.py
@@ -7,3 +7,9 @@ def test_offer_model_parsing():
     name = "get-offer-by-id"
     with raw_fixture(name) as fixture:
         assert Offer.from_json(fixture["data"])
+
+
+def test_offer_model_parsing_with_nullable_payment_requirements():
+    name = "get-offer-by-id-with-null-payment-requirements"
+    with raw_fixture(name) as fixture:
+        assert Offer.from_json(fixture["data"])
